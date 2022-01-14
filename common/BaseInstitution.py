0548cb044162870f588d0300b3f582ed21c5a4c0
sunxiangyu <1340518678@qq.com>
2022-01-14 16:18:49 +0800

    1.21优化

---
+++ common/BaseInstitution.py
@@ -0,0 +1,31 @@
+import requests
+import jsonpath
+
+base_url = 'http://10.5.6.181:16630'
+
+
+class GetMessage(object):
+
+    def get_highYieldFlag(self):
+        url = base_url + '/v1/fundTrends/institution/list'
+        jsondata = {
+            "averageRate": 5,
+            "totalNetworth": 30
+        }
+
+        response = requests.post(url=url, json=jsondata)
+        res = response.json()
+        li = []
+        for i in res["result"]:
+            # print(i)
+            if i["highYieldFlag"] == 1:
+                li.append(i["fundInsCode"])
+
+        print(li)
+        return li
+
+
+
+if __name__ == '__main__':
+    msg = GetMessage()
+    msg.get_highYieldFlag()
